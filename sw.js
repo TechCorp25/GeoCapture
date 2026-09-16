f6213095512975d334e643cd0bb481ac313fcc94
TechCorp <techcorp2024@gmail.com>
2026-09-16 18:55:52 -0400

    Refresh PWA cache for unnumbered bay mode

--- sw.js
+++ sw.js
@@ -1,7 +1,10 @@
-const CACHE="civicmaps-gps-v1";
+const CACHE="civicmaps-gps-v2";
 const ASSETS=["./","./index.html","./manifest.json"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("activate",e=>e.waitUntil(Promise.all([
+  self.clients.claim(),
+  caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
+])));
 self.addEventListener("fetch",e=>{
   if(e.request.method!=="GET") return;
   e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
